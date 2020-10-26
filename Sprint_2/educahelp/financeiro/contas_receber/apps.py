7ea6412bdccbdfa08ce6f3d24dfc25d56184b5df
unknown <lucasc.a01@outlook.com>
2020-10-26 11:39:41 -0300

    remake

--- Sprint_2/educahelp/financeiro/contas_receber/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ContasReceberConfig(AppConfig):
-    name = 'contas_receber'
--- Sprint_2/educahelp/financeiro/recebimentos/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class RecebimentosConfig(AppConfig):
-    name = 'recebimentos'
