44eb25264d9b0a91aec12247c4853fbf8b640b79
unknown <lucasc.a01@outlook.com>
2020-10-28 23:31:11 -0300

    remove files

--- Sprint_2/educahelp/cadastros/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CadastrosConfig(AppConfig):
-    name = 'cadastros'
--- Sprint_2/educahelp/financeiro/contas_pagar/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ContasPagarConfig(AppConfig):
-    name = 'contas_pagar'
