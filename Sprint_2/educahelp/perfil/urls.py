7547d2f12d1ac79a4b9d96a5fd45627b03131b9d
unknown <lucasc.a01@outlook.com>
2020-11-02 14:52:27 -0300

    remove files

--- Sprint_2/educahelp/perfil/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'perfil'
-
-urlpatterns = [
-    path('', views.criar.as_view(), name='criar'),
-    path('atualizar/', views.atualizar.as_view(), name='atualizar'),
-]
