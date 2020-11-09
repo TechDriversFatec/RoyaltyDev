a00d8ee3eddce7db74d595c36dbc9bab237583e3
unknown <elias.faria@fatec.sp.gov.br>
2020-11-08 23:33:31 -0300

    atualização do perfil

--- Sprint_2/educahelp/perfil/urls.py
+++ Sprint_2/educahelp/perfil/urls.py
@@ -5,7 +5,8 @@ app_name = 'perfil'
 
 urlpatterns = [
     path('', views.Criar.as_view(), name='criar'),
-    path('atualizar/', views.Atualizar.as_view(), name='atualizar'),
+    path('atualizar1/', views.Atualizar1.as_view(), name='atualizar1'),
     path('login/', views.Login.as_view(), name='login'),
     path('logout/', views.Logout.as_view(), name='logout'),
+    path('atualizar/', views.atualizar, name='atualizar'),
 ]
